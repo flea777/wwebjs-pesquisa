38e333abdd8a08f4b454d9103e07af9d7af3d4d8
Pedro Bim <pedrodinizz2@outlook.com>
2024-07-30 12:17:06 -0300

    updates

---
+++ src/messages.js
@@ -0,0 +1,13 @@
+const messages = {
+  start: 'Olá, seja bem vindo(a) a pesquisa eleitoral 2024!',
+  askData: 'Vamos precisar de alguns dados para completar a pesquisa.',
+  askName: 'Favor informar o seu nome:',
+  askCpf: 'Favor informar o seu CPF:',
+  askEmail: 'Favor informar o seu email:',
+  askVote: 'Qual sua intenção de voto? \n' +
+           'Candidato 1: Maria Brown\n' +
+           'Candidato 2: John Green',
+  done: 'Obrigado por votar!'
+};
+
+module.exports = messages;
--- src/server.js
+++ src/server.js
@@ -1,5 +1,9 @@
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
+const axios = require('axios');
+const messages = require('./messages');
+
+const baseUrl = 'http://localhost:8080/message';
 
 const client = new Client({
   authStrategy: new LocalAuth()
@@ -14,16 +18,83 @@ client.on('qr', qr => {
 });
 
 client.on('message_create', message => {
+
+  const isGroup = false;
+  
+  if(message.from.includes('g.us')) {
+    isGroup = true;
+  }
+
+  const body = message.body;
   const user = message.from;
-
-  const isGroup = message.getChat().isGroup;
-
+  const userReplaced = user.replace("@c.us", "");
+  
   if(message.isStatus || isGroup) {
-    console.log('group: ' + message.body); 
+    // console.log('group: ' + body); 
     return;
   } else {
-    console.log('not group: ' + message.body);
+    console.log(userReplaced);
+    console.log('===============');
+    console.log('USUÁRIO ' + user);
+    console.log('MENSAGEM: ' + body);
+    console.log('===============');
   }
+  
+  
+  let getResponseData;
+  
+  const getUrl = `${baseUrl}/${userReplaced}`;
+  console.log(getUrl);
+
+  axios.get(getUrl)
+    .then((response) => {
+      getResponseData = response.data.status;
+      console.log(getResponseData);
+    })
+    .catch((error) => console.log('erro')); 
+
+  
+  if (getResponseData === 'NULL') {
+    return;
+  } else if(getResponseData === 'ASK_NAME') {
+    axios.post(baseUrl, {
+      username: userReplaced,
+      body: body
+    })
+    .then((response) => {
+      console.log(response.data.status);
+      client.sendMessage(user, messages.start);
+      client.sendMessage(user, messages.askData);
+      client.sendMessage(user, messages.askName);
+      return;
+    })
+    .catch((error) => console.log('error'));
+  } else if(getResponseData === 'ASK_CPF' || getResponseData === 'ASK_CPF' || getResponseData === 'ASK_EMAIL' || getResponseData === 'ASK_VOTE'
+    || getResponseData === 'DONE') {
+      axios.put(baseUrl, {
+        username: userReplaced,
+        body: body
+      })
+      .then( (response) => {
+        console.log(response.data.status);
+        if (data === 'ASK_CPF') {
+          client.sendMessage(user, messages.askCpf);
+          return;
+        } else if (data === 'ASK_EMAIL') {
+          client.sendMessage(user, messages.askEmail);
+          return;
+        } else if (data === 'ASK_VOTE') {
+          client.sendMessage(user, messages.askVote);
+          return;
+        } else if (data === 'DONE') {
+          client.sendMessage(user, messages.done);
+          return;
+        }
+        }
+      )
+      .catch((error) => console.log('error'));
+    }
+ 
 }); 
 
 client.initialize();
